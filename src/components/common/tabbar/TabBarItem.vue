b663377b353ab7f4428ea38a5ba72b8ae15d20ee
ffy2020 <292621969@qq.com>
2020-03-28 15:20:40 +0800

    框架搭建

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tab-bar-item" @click="itmeClick">
+    <slot v-if="!IsActive" name="tab-item-icon"></slot>
+    <slot v-else name="tab-item-icon-active"></slot>
+    <div :style="activeStyle">
+      <slot name="tab-item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props:{
+    path:String,
+    activecolor:{
+    type:String,
+    default:'red'
+
+    }
+  },
+  data() {
+    return{
+
+    }
+  },
+  computed:{
+    IsActive(){
+      return  this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle(){
+      return this.IsActive?{color:this.activecolor}:{}
+    }
+  },
+methods:{
+ itmeClick(){
+  this.$router.replace(this.path)
+ }
+
+
+}
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
+
+<style >
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Home">
+      <h2>首页</h2>
+    <div>可乐是个好孩子</div>
+      <img src="~assets/img/tabbar/home.svg" alt />
+  </div>
+</template>
+
+
+<script>
+</script>
+
+<style >
+</style>
